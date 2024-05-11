6ff31c59b9cdccc4dd8d730a26ca514dbefc47b4
s-renren <sakurazawaren@gmail.com>
2024-05-11 21:36:04 +0900

    ボムをランダム配置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,11 +14,42 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
+  const [bombMap, setBombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  const isStart = !bombMap.flat().includes(1);
+
+  const img = 1;
+
   const clickHandler = (x: number, y: number) => {
     const newUserInput = structuredClone(userInput);
-    newUserInput[y][x] = (newUserInput[y][x] + 1) % 4;
-    console.log(newUserInput[y][x]);
-    setUserInput(newUserInput);
+    const newBombMap = structuredClone(bombMap);
+
+    if (newUserInput[y][x] === 0) {
+      if (isStart) {
+        while (newBombMap.flat().filter((num) => num === 1).length < 10) {
+          const randomX = Math.floor(Math.random() * 9);
+          const randomY = Math.floor(Math.random() * 9);
+          if (randomX === x && randomY === y) {
+            continue;
+          } else {
+            newBombMap[randomY][randomX] = 1;
+          }
+        }
+        setBombMap(bombMap);
+      }
+      newUserInput[y][x] = (newUserInput[y][x] + 1) % 4;
+      setUserInput(newUserInput);
+    }
   };
 
   return (
@@ -31,7 +62,12 @@ const Home = () => {
           <div className={styles.board}>
             {userInput.map((row, y) =>
               row.map((color, x) => (
-                <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)} />
+                <div
+                  className={color === 0 ? styles.cell : styles.none}
+                  style={{ backgroundPosition: `${-30 * (img - 1) - 2}px -2px` }}
+                  key={`${x}-${y}`}
+                  onClick={() => clickHandler(x, y)}
+                />
               )),
             )}
           </div>
