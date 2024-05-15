d6b1b9c442d4bd14faa65d27d6abec6c8c3d8e77
s-renren <sakurazawaren@gmail.com>
2024-05-15 16:32:26 +0900

    useStateを1つ減らした。ただし、ボムが上手く表示されない。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const [board, setBoard] = useState([
+  const board: number[][] = [
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -36,39 +36,46 @@ const Home = () => {
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-  ]);
+  ];
 
   const isStart = !bombMap.flat().includes(1);
   const isEnd = board.flat().includes(11);
 
-  const aroundBombNum = (newBoard: number[][], newBombMap: number[][], x: number, y: number) => {
-    newBoard[y][x] = [-1, 0, 1]
-      .map((dx) =>
-        [-1, 0, 1].map(
-          (dy) => newBombMap[y + dy] !== undefined && newBombMap[y + dy][x + dx] === 1,
-        ),
-      )
-      .flat()
-      .filter(Boolean).length;
+  const aroundBombNum = (
+    board: number[][],
+    newBombMap: number[][],
+    userInput: number[][],
+    x: number,
+    y: number,
+  ) => {
+    if (userInput[y][x] !== 2 || userInput[y][x] !== 3) {
+      board[y][x] = [-1, 0, 1]
+        .map((dx) =>
+          [-1, 0, 1].map(
+            (dy) => newBombMap[y + dy] !== undefined && newBombMap[y + dy][x + dx] === 1,
+          ),
+        )
+        .flat()
+        .filter(Boolean).length;
 
-    if (newBoard[y][x] === 0) {
-      [-1, 0, 1].forEach((dx) => {
-        [-1, 0, 1].forEach((dy) => {
-          if (newBoard[y + dy] !== undefined && newBoard[y + dy][x + dx] === -1) {
-            aroundBombNum(newBoard, newBombMap, x + dx, y + dy);
-          }
+      if (board[y][x] === 0) {
+        [-1, 0, 1].forEach((dx) => {
+          [-1, 0, 1].forEach((dy) => {
+            if (board[y + dy] !== undefined && board[y + dy][x + dx] === -1) {
+              aroundBombNum(board, newBombMap, userInput, x + dx, y + dy);
+            }
+          });
         });
-      });
+      }
     }
   };
 
   const clickHandler = (x: number, y: number) => {
     const newUserInput = structuredClone(userInput);
     const newBombMap = structuredClone(bombMap);
-    const newBoard = structuredClone(board);
 
     if (!isEnd) {
-      if (newBoard[y][x] === -1) {
+      if (board[y][x] === -1) {
         if (isStart) {
           while (newBombMap.flat().filter((num) => num === 1).length < 10) {
             const randomX = Math.floor(Math.random() * 9);
@@ -82,19 +89,18 @@ const Home = () => {
           setBombMap(newBombMap);
         }
         if (newBombMap[y][x] !== 1) {
-          newUserInput[y][x] = (newUserInput[y][x] + 1) % 4;
-          aroundBombNum(newBoard, newBombMap, x, y);
+          newUserInput[y][x] = 1;
         } else if (newBombMap[y][x] === 1) {
           newBombMap.forEach((row, x) =>
             row.forEach((n, y) => {
               if (newBombMap[y][x] === 1) {
-                newBoard[y][x] = 11;
-                newUserInput[y][x] = (newUserInput[y][x] + 1) % 4;
+                board[y][x] = 11;
+                newUserInput[y][x] = 1;
+                console.log(board[y][x]);
               }
             }),
           );
         }
-        setBoard(newBoard);
         setUserInput(newUserInput);
       }
     }
@@ -102,16 +108,31 @@ const Home = () => {
 
   const clickRight = (x: number, y: number, event: React.MouseEvent) => {
     event.preventDefault();
-    const newBoard = structuredClone(board);
-    if (newBoard[y][x] === -1 || newBoard[y][x] === 9 || newBoard[y][x] === 10) {
-      newBoard[y][x] === -1
-        ? (newBoard[y][x] = 9)
-        : newBoard[y][x] === 9
-          ? (newBoard[y][x] = 10)
-          : (newBoard[y][x] = -1);
+    const newUserInput = structuredClone(userInput);
+    if (newUserInput[y][x] === 0 || newUserInput[y][x] === 2 || newUserInput[y][x] === 3) {
+      newUserInput[y][x] === 0
+        ? (newUserInput[y][x] = 2)
+        : newUserInput[y][x] === 2
+          ? (newUserInput[y][x] = 3)
+          : (newUserInput[y][x] = 0);
     }
-    setBoard(newBoard);
+    console.log(newUserInput[y][x]);
+    setUserInput(newUserInput);
   };
+
+  userInput.forEach((row, dy) => {
+    row.forEach((isClick, dx) => {
+      if (isClick === 1) {
+        aroundBombNum(board, bombMap, userInput, dx, dy);
+      } else if (isClick === 2 || isClick === 3) {
+        isClick === 2
+          ? (board[dy][dx] = 9)
+          : isClick === 3
+            ? (board[dy][dx] = 10)
+            : (board[dy][dx] = -1);
+      }
+    });
+  });
 
   return (
     <div className={styles.container}>
@@ -130,8 +151,8 @@ const Home = () => {
                         number === -1
                           ? `${-30 * (userInput[y][x] - 1)}px 0px`
                           : number === 9 || number === 10
-                            ? `${-30 * (board[y][x] - 1)-2}px -3px`
-                            : `${-30 * (board[y][x] - 1)+2}px 0px`,
+                            ? `${-30 * (board[y][x] - 1) - 2}px -3px`
+                            : `${-30 * (board[y][x] - 1) + 2}px 0px`,
                     }}
                     className={
                       number === -1
